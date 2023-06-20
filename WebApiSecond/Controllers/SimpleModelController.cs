1e6d52f392d061464cb0d8868987b87af5fa4b12
nacharvs <r3dinc@yandex.ru>
2023-06-20 13:41:53 +0300

    start

---
+++ WebApiSecond/Controllers/SimpleModelController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiSecond.Models;
+
+namespace WebApiSecond.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SimpleModelController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllModels()
+        {
+           return Ok(SimpleModel.GetModels());
+        }
+    }
+}
---
+++ WebApiSecond/Models/SimpleModel.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApiSecond.Models
+{
+    public class SimpleModel
+    {
+        public SimpleModel(string? name, int age, string? eMail)
+        {
+            Name = name;
+            Age = age;
+            EMail = eMail;
+        }
+
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? EMail { get; set; }
+
+        public static List<SimpleModel> GetModels()
+        {
+            var list = new List<SimpleModel>();
+            list.Add(new SimpleModel("vadim", 33, "vadim@mail.ru"));
+            list.Add(new SimpleModel("Victor", 22, "victor@mail.ru"));
+            return list;
+        }
+    }
+}
